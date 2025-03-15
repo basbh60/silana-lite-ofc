2f5daffeecabd52b49dae249c998607213656650
basbh60 <mashal78197@gmail.com>
2025-03-15 16:45:10 +0300

    Update settings.js

--- function/settings/settings.js
+++ function/settings/settings.js
@@ -14,12 +14,12 @@ global.setting = {
  }
 
 global.info = {
- nomerbot : '212605784394',
- pairingNumber : '212605784394',
+ nomerbot : '967781974099',
+ pairingNumber : '967781974099',
  figlet: 'silanalite', // create a start console display
- nomorwa : '212605784394',
+ nomorwa : '967781974099',
  nameown : 'NOUREDDINE',
- nomerown : '212717457920',
+ nomerown : '967781974099',
  packname : 'sticker by ',
  author : 'SILANA AI',
  namebot : '乂 SILANA AI',
